baea97545917ffe7b9c63ea1bfd573fbd6eed5cc
Kritika Shaw <shawkritika2@gmail.com>
2024-01-05 19:41:31 +0530

    Update settings.py

--- django_todo_app/settings.py
+++ django_todo_app/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%(zt!thth9wpy5s*dt3_*4oxwn9chpc4dt!-ee9qoad92q0nq%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
